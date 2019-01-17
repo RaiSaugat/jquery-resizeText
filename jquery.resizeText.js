72d1b617745fc51b4430a25e5a4ee3b8564061fe
raisaugat <rai.saugat.sr@gmail.com>
2019-01-17 13:13:56 +0545

    Functionality added for dynamic height

--- jquery.resizeText.js
+++ jquery.resizeText.js
@@ -1,13 +1,23 @@
 (function($) {
-	$.fn.resizeText = function() {
-		var node = $(this);
-		node.text(resizeText($(this).css('font-size')));
-		function resizeText(fontSize) {
-			node.css('fontSize', parseFloat(fontSize) - 1);
-			node.css('opacity', 1);
-				if (node.height() >= (node.parent()).height()) {
-					resizeText(node.css('font-size'));
+	$.fn.extend({
+		resizeText: function(options) {
+			return this.each(function() {
+				var defaults = {
+					height: "80",
+				};
+				options = $.extend(defaults, options);
+				var height = options.height + "px";
+				var node = $(this);
+				node.parent().css('height', height)
+				node.text(resizeTextSize($(this).css('font-size')));
+				function resizeTextSize(fontSize) {
+					node.css('fontSize', parseFloat(fontSize) - 1);
+					node.css('opacity', 1);
+						if (node.height() >= (node.parent()).height()) {
+							resizeTextSize(node.css('font-size'));
+						}
 				}
+			})
 		}
-	}
+	})
 } (jQuery));
